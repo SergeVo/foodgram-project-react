1b8a15e4ae0dfb4c211ff99d2f4755b01f2e21a7
Сергей Догадин <me@sdogadin.ru>
2023-09-26 17:25:32 +0300

    fixes_flake_isort

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -1,8 +1,7 @@
 import json
 
 from django.core.management.base import BaseCommand
-
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -2,7 +2,6 @@ from colorfield.fields import ColorField
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 from django.db.models import UniqueConstraint
-
 from users.models import User
 
 
